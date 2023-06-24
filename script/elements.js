6158755273eebf3598bebdf934abc162dab43820
Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>
2023-06-24 13:47:38 -0600

    Use <br-l>
    Signed-off-by: Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>

--- script/elements.js
+++ script/elements.js
@@ -1,11 +1,13 @@
-class brL extends HTMLElement {
+class BrL extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `<br>`;
-        this.style = {
-          border-bottom: 1px solid black;
-        }
+        this.style.display = 'block';
+        this.style.height = '1px';
+        this.style.margin = '0';
+        this.style.padding = '0';
+        this.style.borderTop = '1px solid black';
+        this.style.borderBottom = '1px solid black';
     }
 }
 
-window.customElements.define('br-l', brL);
-//use <br-l></br-l>
+window.customElements.define('br-l', BrL);
+//use <br-l>
