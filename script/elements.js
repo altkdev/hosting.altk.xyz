b57919c3fc0ea3ae4e6ecb0b82f1ba64d6551a2a
Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>
2023-06-24 14:52:34 -0600

    You have to close the tags again
    Signed-off-by: Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>

--- script/elements.js
+++ script/elements.js
@@ -1,4 +1,26 @@
 class BrL extends HTMLElement {
+    connectedCallback() {
+        const height = this.getAttribute('height') || '1px';
+        const borderHeight = `calc(${height} * 3 / 5)`;
+        this.style.display = 'block';
+        this.style.height = height;
+        this.style.margin = '0';
+        this.style.padding = '0';
+        this.style.borderTop = `${borderHeight} solid #dadcdc`;
+        this.style.borderBottom = 'none';
+    }
+    static get observedAttributes() {
+        return ['height'];
+    }
+    attributeChangedCallback() {
+        // Reapply the styles when the height attribute changes
+        this.connectedCallback();
+    }
+}
+
+window.customElements.define('br-l', BrL);
+
+class BrL extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
