f73b45f4415fc5422e5caab8ddc1fcee4c4df261
Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>
2023-06-24 14:42:22 -0600

    Update elements.js
    Signed-off-by: Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>

--- script/elements.js
+++ script/elements.js
@@ -1,24 +1,53 @@
 class BrL extends HTMLElement {
-    connectedCallback() {
-        const height = this.getAttribute('height') || '1px';
-        const borderHeight = `calc(${height} * 3 / 5)`;
-        this.style.display = 'block';
-        this.style.height = height;
-        this.style.margin = '0';
-        this.style.padding = '0';
-        this.style.borderTop = `${borderHeight} solid #dadcdc`;
-        this.style.borderBottom = 'none';
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
 
-    // Override the HTMLElement behavior to make the element self-closing
-    static get observedAttributes() {
-        return ['height'];
-    }
+  connectedCallback() {
+    const height = this.getAttribute('height') || '1px';
+    const borderHeight = `calc(${height} * 3 / 5)`;
 
-    attributeChangedCallback() {
-        // Reapply the styles when the height attribute changes
-        this.connectedCallback();
-    }
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          height: ${height};
+          margin: 0;
+          padding: 0;
+          background: linear-gradient(to bottom, transparent, #dadcdc, transparent);
+          box-sizing: border-box;
+        }
+        
+        :host:before,
+        :host:after {
+          content: '';
+          display: block;
+          height: ${borderHeight};
+          background-color: #dadcdc;
+          margin: auto;
+        }
+
+        :host:before {
+          margin-bottom: ${borderHeight};
+        }
+
+        :host:after {
+          margin-top: ${borderHeight};
+        }
+      </style>
+    `;
+  }
+
+  // Override the HTMLElement behavior to make the element self-closing
+  static get observedAttributes() {
+    return ['height'];
+  }
+
+  attributeChangedCallback() {
+    // Reapply the styles when the height attribute changes
+    this.connectedCallback();
+  }
 }
 
 window.customElements.define('br-l', BrL, { selfClosing: true });
