10043e76111efb1547c9337812a9a3c5cc2ac429
Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>
2023-06-24 14:27:29 -0600

    Look at desc
    Include the <br-l> tag in your HTML markup where you want the line break.
    Optionally, add the height attribute to customize the height of the line break. For example, <br-l height="10px" /> will create a line break with a height of 10px.
    The <br-l> tag will automatically apply the specified height and style, including a top border color of #dadcdc 
    
    Signed-off-by: Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>

--- script/elements.js
+++ script/elements.js
@@ -1,13 +1,24 @@
 class BrL extends HTMLElement {
     connectedCallback() {
+        const height = this.getAttribute('height') || '1px';
+        const borderHeight = `calc(${height} * 3 / 5)`;
         this.style.display = 'block';
-        this.style.height = '1px';
+        this.style.height = height;
         this.style.margin = '0';
         this.style.padding = '0';
-        this.style.borderTop = '1px solid black';
-        this.style.borderBottom = '1px solid black';
+        this.style.borderTop = `${borderHeight} solid #dadcdc`;
+        this.style.borderBottom = 'none';
+    }
+
+    // Override the HTMLElement behavior to make the element self-closing
+    static get observedAttributes() {
+        return ['height'];
+    }
+
+    attributeChangedCallback() {
+        // Reapply the styles when the height attribute changes
+        this.connectedCallback();
     }
 }
 
-window.customElements.define('br-l', BrL);
-//use <br-l>
+window.customElements.define('br-l', BrL, { selfClosing: true });
