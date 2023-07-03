3e935d319dbcc93dc56e3d9c3d4a9b84c49c259d
Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>
2023-07-02 19:36:24 -0600

    invis
    Signed-off-by: Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>

--- script/elements.js
+++ script/elements.js
@@ -6,10 +6,12 @@ class brL extends HTMLElement {
         this.style.height = height;
         this.style.margin = '0';
         this.style.padding = '0';
-        this.style.borderBottom = `${borderHeight} solid #6f0be0`;
+        if (!this.hasAttribute("invis")) {
+            this.style.borderBottom = `${borderHeight} solid #6f0be0`;
+        }    
     }
     static get observedAttributes() {
-        return ['height'];
+        return ['height', 'invis'];
     }
     attributeChangedCallback() {
         // Reapply the styles when the height attribute changes
